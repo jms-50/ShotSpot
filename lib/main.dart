411a0ad3fd374cd8d620c2892f8d792e3f86744c
ms50 <83538060+jms-50@users.noreply.github.com>
2025-06-04 14:45:29 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(),
       home: const AuthGate(),
+
       routes: {
         '/main':(context) => const MainScreen(),
         '/camera':(context) => const CameraScreen(),
